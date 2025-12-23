46ceb91f3daa64d0bf5147c3943340553846f1f2
YilinMacAir <yilin7yilin@gmail.com>
2025-12-23 23:12:30 +0800

    fix go vet bug

--- gapi/rpc_create_user.go
+++ gapi/rpc_create_user.go
@@ -48,7 +48,7 @@ func (server *Server) CreateUser(ctx context.Context, req *pb.CreateUserRequest)
 	txResult, err := server.store.CreateUserTx(ctx, arg)
 	if err != nil {
 		if db.ErrorCode(err) == db.UniqueViolation {
-			return nil, status.Errorf(codes.AlreadyExists, err.Error())
+			return nil, status.Errorf(codes.AlreadyExists, "duplicate key: %s", err)
 		}
 		return nil, status.Errorf(codes.Internal, "failed to create user: %s", err)
 	}
